19a6d6e5160ccc55cd1a36eeecb749eb73dbce41
Guido Ising <guido.ising@freenet.de>
2025-07-16 19:03:45 +0200

    Updated Notification with repository name - bugfix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ auth_token = os.environ['auth_token']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Push event triggered in master branch of repository:'+os.environ['repository_name']+'\n'+os.environ['commit_message'],
+                              body='Push event triggered in master branch of repository: '+os.environ['repository_name']+'\n'+os.environ['commit_message'],
                               from_='whatsapp:+14155238886',
                               to='whatsapp:'+os.environ['to_whatsapp_no']
                           )
