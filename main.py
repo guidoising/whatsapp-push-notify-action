d258a9b872fe889c0f38dce8b195675540a4869e
Guido Ising <guido.ising@freenet.de>
2025-07-16 18:21:28 +0200

    Changed Notify Message and uppercase for Variables

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ['account_sid']
-auth_token = os.environ['auth_token']
+account_sid = os.environ['ACCOUNT_SID']
+auth_token = os.environ['AUTH_TOKEN']
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Yay! Push event triggered in master branch',
+                              body='Push event triggered in master branch',
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:'+os.environ['to_whatsapp_no']
+                              to='whatsapp:'+os.environ['TO_WHATSAPP_NO']
                           )
 
 print("Message ID:",message.sid)
